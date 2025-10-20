4fdeca99087688bf536f2ca7b209318e9e6c610c
Abdel Mourid <abdelmouridelmrabit07@gmail.com>
2025-10-20 18:49:12 +0000

    o

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -8,7 +9,7 @@
  */
 int main(void)
 {
-	char ch = 'a b c d e f g h i j k l m n o p q r s t u v w x y z';
+	char ch = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
 	putchar(ch);
 	return (0);
 }
