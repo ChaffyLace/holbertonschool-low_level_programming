e661b09cccec7346a64a0ee9eeaa475cc325dec3
Abdel Mourid <abdelmouridelmrabit07@gmail.com>
2025-10-20 18:05:31 +0000

    o

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -14,12 +14,12 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf(n,"is positive");
+		printf("is positive",n);
 	}
 	else 
 		if (n < 0)
-			printf(n,"is negative");
+			printf("is negative",n);
 		else
-			printf(n,"is zero");
+			printf("is zero",n);
 	return (0);
 }
