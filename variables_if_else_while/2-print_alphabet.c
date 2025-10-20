86ad975d768727e9dcd31309f9e6ea5c2b2d0a71
Abdel Mourid <abdelmouridelmrabit07@gmail.com>
2025-10-20 18:44:49 +0000

    o

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch ='a b c d e f g h i j k l m n o p q r s t u v w x y z'
+	putchar(ch);
+	return (0);
+}
