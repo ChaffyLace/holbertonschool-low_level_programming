54269f16de84b7f7de27f5986e6e194f2c9edfd0
Abdel Mourid <abdelmouridelmrabit07@gmail.com>
2025-10-20 18:57:53 +0000

    o

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -9,7 +9,9 @@
  */
 int main(void)
 {
-	char ch = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
-	putchar(ch);
+	char ch;
+	for (ch = 'a', ch <= 'z'; ch++);
+		putchar(ch);
+	putchar('\n'); 
 	return (0);
 }
