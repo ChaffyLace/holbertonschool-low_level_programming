fee29eb863af494a4f6ba2281f8f45054105db53
abdelmourid <abdelmouridelmrabit07@gmail.com>
2025-10-21 18:00:18 +0000

    o

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -14,8 +14,9 @@ int main(void)
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
 
-	for (ch = 'a'; ch <= 'z'; ch++)
+	for (ch = 'A'; ch <= 'Z'; ch++)
                 putchar(ch);
+
 	putchar('\n');
 	return (0);
 }
