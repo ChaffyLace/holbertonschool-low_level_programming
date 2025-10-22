67742ce8791e5f8b6fc7f92e347a07c6c81c720b
abdelmourid <abdelmouridelmrabit07@gmail.com>
2025-10-22 08:26:03 +0000

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	for (i = 0; i <= 9)
+	{
+		putchar(i + "0");
+	}
+	putchar('\n');
+	return (0);
+}
