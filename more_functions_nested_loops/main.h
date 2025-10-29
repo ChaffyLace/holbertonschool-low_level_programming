60fd33e9752cd7084db37a67161a777148b987c1
abdel <abdelmouridelmrabit07@gmail.com>
2025-10-29 18:14:17 +0000

    o

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,13 +1,8 @@
-#include <stdio.h>
-#ifndef
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 int _isupper(int c);
 
 #endif
+
