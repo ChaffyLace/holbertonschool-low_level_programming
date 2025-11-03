152ee18e64f144c4b780206e21f1ac6fd2f0c53c
abdel <abdelmouridelmrabit07@gmail.com>
2025-11-03 12:30:27 +0000

    o

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,14 @@
+#include "Main.h"
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	unsigned int i;
+	unsigned char *k = dest;
+	const unsigned char *p = src;
+
+	for(i = 0; i < n; i++);
+	k[i] = p[i];
+Â
+	return (dest);
+}
+
