94d61ffa7af352e688e933ad9d218f8d8108e4a3
abdel <abdelmouridelmrabit07@gmail.com>
2025-11-03 12:35:02 +0000

    o

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,5 +1,12 @@
 #include "Main.h"
 
+/**
+ * _memcpy() - checks for uppercase character
+ * @c: the character to check
+ *
+ * Return: dest if c is uppercase, 0 otherwis
+ */
+
 char *_memcpy(char *dest, char *src, unsigned int n);
 {
 	unsigned int i;
