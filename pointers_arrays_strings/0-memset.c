3915fa9a19e15aaa57a5ae78ebb4538786c5a05c
abdel <abdelmouridelmrabit07@gmail.com>
2025-11-03 10:45:20 +0000

    o

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * 0-memset.c - a function that fills memory with a constant byte.
+ *
+ * Returns a pointer to the memory area s
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
