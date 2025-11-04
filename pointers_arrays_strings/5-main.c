1bb0b48c93eb10930f131ffefee5474ff9f9a2d8
abdel <abdelmouridelmrabit07@gmail.com>
2025-11-04 13:34:43 +0000

    o

--- pointers_arrays_strings/5-main.c
+++ pointers_arrays_strings/5-main.c
@@ -2,16 +2,17 @@
 #include <stdio.h>
 
 /**
- * main - teste la fonction rev_string
+ * main - check the code
  *
- * Return: toujours 0
+ * Return: Always 0.
  */
 int main(void)
 {
-	char s[10] = "My School";
-    
-    printf("%s\n", s);
-	rev_string(s);
-	printf("%s\n", s);
-	return (0);
+    char *s = "hello, world";
+    char *f = "world";
+    char *t;
+
+    t = _strstr(s, f);
+    printf("%s\n", t);
+    return (0);
 }
