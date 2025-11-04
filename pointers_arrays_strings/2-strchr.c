1b34dc2bb1d3a66cd5dec86de3e3192b37087a8c
abdel <abdelmouridelmrabit07@gmail.com>
2025-11-04 10:40:19 +0000

    o

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -2,12 +2,9 @@
 #include <stddef.h>
 
 /**
- * _strchr - locates a character in a string
- * @s: pointer to the string to search in
- * @c: character to find
+ * main - check the code
  *
- * Return: pointer to the first occurrence of c in s,
- * or NULL if the character is not found
+ * Return: Always 0.
  */
 char *_strchr(char *s, char c)
 {
---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+unsigned int _strspn(char *s, char *accept) {
+    unsigned int cpt = 0;
+    char *a;
+
+    while (*s != '\0') {
+        a = accept;
+        int found = 0;
+
+        while (*a != '\0') {
+            if (*s == *a) {
+                found = 1; 
+                break;
+            }
+            a++;
+        }
+
+        if (found == 0) 
+            break; 
+
+        cpt++; 
+        s++; 
+    }
+    return cpt; 
+}
