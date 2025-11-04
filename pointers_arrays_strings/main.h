a68a00e73080e511a6d1c01d3f318a31855b659e
ChaffyLace <132900529+ChaffyLace@users.noreply.github.com>
2025-11-04 10:12:44 +0100

    Add files via upload

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,8 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-
-#endif
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _putchar(char c);
+int _strlen(char *s);
+void _puts(char *str);
+int _putchar(char c);
+void print_rev(char *s);
+void rev_string(char *s);
+int _putchar(char c);
+void puts2(char *str);
+int _putchar(char c);
+void puts_half(char *str);
+void print_array(int *a, int n);
+int _atoi(char *s);
+#endif /* MAIN_H */
