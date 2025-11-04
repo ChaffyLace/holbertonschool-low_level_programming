a523cd4d3ae4ea18eb691fdfbec054607e63cd1c
abdel <abdelmouridelmrabit07@gmail.com>
2025-11-04 10:59:37 +0000

    o

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -5,27 +5,34 @@
  *
  * Return: Always 0.
  */
-unsigned int _strspn(char *s, char *accept) {
+unsigned int _strspn(char *s, char *accept)
+{
 	unsigned int cpt = 0;
+	int found;
 	char *a;
-	int found = 0;
 
-    while (*s != '\0') {
-        a = accept;
+	while (*s != '\0')
+	{
+		found = 0;
+		a = accept;
 
-        while (*a != '\0') {
-            if (*s == *a) {
-                found = 1;
-                break;
-            }
-            a++;
-        }
+		while (*a != '\0')
+		{
+			if (*s == *a)
+			{
+				found = 1;
+				break;
+			}
+			a++;
+		}
 
-        if (found == 0)
-            break;
+		if (found == 0)
+			break;
 
-        cpt++;
-        s++;
-    }
-    return cpt;
+		cpt++;
+		s++;
+	}
+
+	return (cpt);
 }
+
