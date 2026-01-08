346419572947a72c2c437e6625adf9a0498eafad
ChaffyLace <132900529+ChaffyLace@users.noreply.github.com>
2026-01-08 23:35:19 +0100

    Refactor variadic_functions.h for better structure

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,21 +1,25 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
+#ifndef _HEADER_
+#define _HEADER_
 #include <stdarg.h>
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_a_char(char *separator, va_list args);
+void print_a_integer(char *separator, va_list args);
+void print_a_float(char *separator, va_list args);
+void print_a_char_ptr(char *separator, va_list args);
 
 /**
- * struct type_finder - find type to right function
- *
- * @s: string
- * @f: fptr
+ * struct format_types - Struct format_types
+ * @identifier: The conversion specifier
+ * @f: The function pointer
  */
-typedef struct type_finder
+typedef struct format_types
 {
-	char *s;
-	void (*f)(va_list ap);
-} ty_f;
+	char *identifier;
+	void (*f)(char *separator, va_list args);
+} f_dt;
 #endif
