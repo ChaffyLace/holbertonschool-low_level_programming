6e3599561127b14462b6cdf012a3bded1c709d2f
ChaffyLace <132900529+ChaffyLace@users.noreply.github.com>
2026-01-08 23:22:30 +0100

    Refactor operation functions with improved comments

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -3,65 +3,45 @@
 #include <stdlib.h>
 
 /**
- * op_add - add
- * @a: a
- * @b: b
- * Return: Res
+ * op_add - adds two integers
  */
-
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
- * op_sub - sub
- * @a: a
- * @b: b
- * Return: Res
+ * op_sub - subtracts two integers
  */
-
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
 /**
- * op_mul - mul
- * @a: a
- * @b: b
- * Return: Res
+ * op_mul - multiplies two integers
  */
-
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
 /**
- * op_div - div
- * @a: a
- * @b: b
- * Return: Res
+ * op_div - divides two integers
  */
-
 int op_div(int a, int b)
 {
 	if (b == 0)
 	{
 		printf("Error\n");
-			exit(100);
+		exit(100);
 	}
 	return (a / b);
 }
 
 /**
- * op_mod - mod
- * @a: a
- * @b: b
- * Return: Res
+ * op_mod - modulo of two integers
  */
-
 int op_mod(int a, int b)
 {
 	if (b == 0)
