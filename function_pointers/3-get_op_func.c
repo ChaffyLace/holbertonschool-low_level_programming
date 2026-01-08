ac8258b04bd4253ba46192138c9223b26ef714a7
ChaffyLace <132900529+ChaffyLace@users.noreply.github.com>
2026-01-08 12:49:09 +0100

    Update 3-get_op_func.c

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -7,15 +7,26 @@
  * @s: operator
  * Return: 0 or res
  */
+
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (ops[i].op[0] == *s)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 }
